d4693fc579af3e35f95de3564c7b62cbdd5e76e6
rahulbanik98 <59626411+rahulbanik98@users.noreply.github.com>
2023-08-31 20:02:14 +0530

    first day

---
+++ src/components/HeroBanner.js
@@ -0,0 +1,68 @@
+import { Box, Typography, Button } from '@mui/material'
+import React from 'react'
+import banner from '../assets/images/banner.png'
+
+function HeroBanner() {
+    return (
+        <Box
+            sx={{
+                mt: { lg: '212px', xs: '70px' },
+                ml: { sm: '50px' }
+            }}
+            position='relative'
+            p='20px'
+        >
+            <Typography
+                color='#FF2625'
+                fontWeight='600'
+                fontSize='26px'
+            >
+                Fitness Club
+            </Typography>
+            <Typography
+                fontWeight='700'
+                mb='23px'
+                mt='30px'
+                sx={{ fontSize: { lg: '44px', xs: '40px' } }}
+            >
+                Sweat, Smile <br /> Repeat
+            </Typography>
+            <Typography
+                fontSize='22px'
+                lineHeight='35px'
+                mb={4}
+            >
+                Checkout The most effective expercises
+            </Typography>
+            <Button
+                variant='contained'
+                color='error'
+                href='#exercises'
+                sx={{
+                    backgroundColor: "#ff2625",
+                    padding: '10px'
+                }}
+            >
+                Explore Expercises
+            </Button>
+            <Typography
+                fontWeight={600}
+                color='#FF2625'
+                fontSize='200px'
+                sx={{
+                    opacity: 0.1,
+                    display: { lg: 'block', xs: 'none' }
+                }}
+            >
+                Exercises
+            </Typography>
+            <img
+                src={banner}
+                alt='banner'
+                className='hero-banner-img'
+            />
+        </Box>
+    )
+}
+
+export default HeroBanner
