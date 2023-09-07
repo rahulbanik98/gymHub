01c73d5180f8d3b20ec4d952e4fa553d27a24746
rahulbanik98 <59626411+rahulbanik98@users.noreply.github.com>
2023-09-08 01:45:27 +0530

    before Final

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 https://www.youtube.com/watch?v=KBpoBc98BwM&ab_channel=JavaScriptMastery
 
-118
+200
--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -12,11 +12,20 @@ export const fetchData = async (url, options) => {
     return data;
 }
 
-const options = {
+// const options = {
+//     method: 'GET',
+//     url: 'https://exercisedb.p.rapidapi.com/exercises/bodyPartList',
+//     headers: {
+//       'X-RapidAPI-Key': '860885445emsh08af5914ad1649fp119627jsn8c2c85f21017',
+//       'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com'
+//     }
+//   };
+
+export const youtubeOptions = {
     method: 'GET',
-    url: 'https://exercisedb.p.rapidapi.com/exercises/bodyPartList',
     headers: {
-      'X-RapidAPI-Key': '860885445emsh08af5914ad1649fp119627jsn8c2c85f21017',
-      'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com'
+      'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
+      'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com'
     }
   };
+  
